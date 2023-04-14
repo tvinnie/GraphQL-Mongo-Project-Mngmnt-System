d67db1e20d059df455249ce80da709dd2b27094a
tvinnie <tvinnie77@gmail.com>
2023-04-14 13:38:48 +0300

    project and client queries

---
+++ server/schema/schema.js
@@ -0,0 +1,75 @@
+// const  { projects, clients } = require('../sampleData.js');
+
+//Moongoose models
+const Project = require('../models/Project.js');
+const Client = require('../models/Client.js');
+
+
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require('graphql')
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+    name:'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString }
+    })
+})
+
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name:'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args){
+                return clients.findById(parent.clientId)
+            }
+        }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        //Projects
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return Project.find();
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return Project.findById(args.id);
+            }
+        },
+        //Clients
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return Client.find();
+            }
+        },
+        client: {
+            type: ClientType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return Client.findById(args.id);
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema ({
+    query: RootQuery
+})
