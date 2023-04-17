a5727b8c60c5fac895a0ec72d916a0f15c940481
tvinnie <tvinnie77@gmail.com>
2023-04-17 17:11:15 +0300

    added client endpoint and set up views

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 require('dotenv').config();
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db')
@@ -12,6 +13,8 @@ const app = express();
 
 //Connect to DB
 connectDB();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
             schema,
